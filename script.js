812ee79fd0ad68cbde5ccf3622d8f7694633cb49
Brandon Kolle <bk584@drexel.edu>
2019-06-10 20:25:19 -0400

    Everything is broken and all hope is lost

--- script.js
+++
@@ -1,8 +0,0 @@
-while(true)
-{
-    var spans = document.querySelectorAll("span[style='color: rgb(0, 180, 0);']");
-    
-    Array.from(spans).forEach(ele => {
-        console.log(ele);
-    });
-}
