2f26a7211f6de694d62f6c38fb373f81710de9a4
Brandon Kolle <bk584@drexel.edu>
2019-06-19 16:47:10 -0400

    Added readme

---
+++ README.md
@@ -0,0 +1,3 @@
+A simple Chrome Extension to find all suggestions in the Superdesk editor and make them more visible.
+
+Author: The Triangle IT Team
--- script.js
+++ script.js
@@ -2,9 +2,9 @@ var spansAdd = document.querySelectorAll("span[style='color: rgb(0, 180, 0);']")
 var spansDel = document.querySelectorAll("span[style='color: rgb(255, 0, 0); text-decoration: line-through;']");
 
 spansAdd.forEach((ele) => {
-ele.setAttribute("style", "color: green; border-bottom: 2px solid green;");
+    ele.setAttribute("style", "color: green; border-bottom: 2px solid green;");
 });
 
 spansDel.forEach((ele) => {
-ele.setAttribute("style", "color: rgb(255, 0, 0); text-decoration: line-through; border-bottom: 2px solid red");
+    ele.setAttribute("style", "color: rgb(255, 0, 0); text-decoration: line-through; border-bottom: 2px solid red");
 });
